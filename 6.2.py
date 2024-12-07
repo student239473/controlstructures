e90c447e4c9c0139fa95b2d9dda11f2ce94a5b11
student239473 <239473@student.uek.krakow.pl>
2024-12-07 07:51:17 +0100

    Add files via upload

---
+++ 6.2.py
@@ -0,0 +1,13 @@
+text = "This is a sample text."
+vowels = "aeiouAEIOU"
+vowel_count = 0
+
+
+index = 0
+
+
+while index < len(text):
+    if text[index] in vowels:  
+        vowel_count += 1
+    index += 1  
+print(f"The number of vowels in the text is: {vowel_count}")
