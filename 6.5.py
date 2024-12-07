5aeb2f4946b2bb07ab1cdc17b690c42af55c22f9
student239473 <239473@student.uek.krakow.pl>
2024-12-07 08:32:38 +0100

    Add files via upload

---
+++ 6.5.py
@@ -0,0 +1,13 @@
+
+temperature = int(input("Enter the temperature in degrees Celsius: "))
+
+if temperature > 35:
+    print("It is extremely hot")
+elif temperature > 30:
+    print("It is hot")
+elif temperature >= 15:
+    print("It is warm")
+elif temperature >= 0:
+    print("It is cold")
+else:
+    print("Warning, frost")
